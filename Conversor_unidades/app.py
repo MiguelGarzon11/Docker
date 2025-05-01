2a64c4cf2a4716ff95c81b738ad2ab6cc7ae8e7a
Miguel Angel <miguelangelgarb@gmail.com>
2025-05-01 17:33:43 -0500

    Pagina de conversor de unidades

--- Conversor_unidades/app.py
+++ Conversor_unidades/app.py
@@ -14,10 +14,37 @@ def index():
 
 @unidades.route("/procesar", methods=["POST"])
 def home():
-    valor_str = request.form.get('valor')
+    valor = float(request.form.get('valor'))
     unidad = request.form.get('unidad')
+    opcion = request.form.get('opcion')
 
-    if valor_str is None or unidad is None:
-        return "Por favor proporciona los parámetros 'valor' y 'unidad'."
+    if valor is None or unidad is None or opcion is None:
+        return "Por favor proporciona los parámetros 'valor', 'unidad' o 'opcion'."
 
-    valor = float(valor_str)
+    if unidad == opcion:
+        return "El valor que intenta convertir ya se encuentra en la unidad esperada."
+    if unidad == 'cm' and opcion == 'm':
+        resultado = f"El valor en metros de {valor}cm es de: {valor/100} metros."
+    elif unidad == 'cm' and opcion == 'ft':
+        resultado = f"El valor en pies de {valor}cm es de: {valor/30.48} pies."
+    elif unidad == 'cm' and opcion == 'in':
+        resultado = f"El valor en pulgadas de {valor}in es de: {valor/2.54} pulgadas."
+
+    elif unidad == 'ft' and opcion == 'cm':
+        resultado = f"El valor en centimetros de {valor}ft es de: {valor * 30.48}centimetros."
+    elif unidad == 'ft' and opcion == 'm':
+        resultado = f"El valor en metros de {valor}ft es de: {valor / 3.28084}metros."
+    elif unidad == 'ft' and opcion == 'in':
+        resultado = f"El valor en pulgadas de {valor}ft es de: {valor * 12}pulgadas."
+    
+    elif unidad == 'in' and opcion == 'cm':
+        resultado = f"El valor en centimetros de {valor}in es de: {valor * 2.54}centimetros."
+    elif unidad == 'in' and opcion == 'm':
+        resultado = f"El valor en metros de {valor}in es de: {valor / 39.3701}metros."
+    elif unidad == 'in' and opcion == 'ft':
+        resultado = f"El valor en pies de {valor}in es de: {valor / 12}pies."
+    
+    return render_template("index.html", resultado=resultado)
+
+if __name__ == '__main__':
+    unidades.run(host='0.0.0.0', port=5000)
